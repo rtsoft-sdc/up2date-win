2670e49fdbc918a9e116d4b889e808e3c141811d
Steamdragon <98mirant@gmail.com>
2022-08-08 23:00:15 +0300

    Update build.bat

--- Up2dateService/wrapperdll/build.bat
+++ Up2dateService/wrapperdll/build.bat
@@ -1,12 +1,13 @@
 echo %1
-echo %2
 cp .\up2date-cpp\vcpkg.json .
 %VCPKG_ROOT%\vcpkg.exe install
-mkdir build_%2
-if %2==x64 (
-    cmake -B build_%2 -S . –A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows
-    cmake --build .\build_%2\. --config %1
-) else (
-    cmake -B build_%2 -S . –A WIN32 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows
-    cmake --build .\build_%2\. --config %1
-)
+
+rm -r build_x64
+mkdir build_x64
+cmake -B build_x64 -S . –A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows
+cmake --build .\build_x64\. --config "Release"
+
+rm -r build_x86
+mkdir build_x86
+cmake -B build_x86 -S . –A WIN32 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows
+cmake --build .\build_x86\. --config "Release"
