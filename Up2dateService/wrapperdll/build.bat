a8d36f0cdd195138095b7240037f8523afa260fb
SteamDragon <98mirant@gmail.com>
2022-08-09 13:27:47 +0300

    Fixes for build.bat

--- Up2dateService/wrapperdll/build.bat
+++ Up2dateService/wrapperdll/build.bat
@@ -1,13 +1,13 @@
 echo %1
-cp .\up2date-cpp\vcpkg.json .
+robocopy .\up2date-cpp\vcpkg.json .
 %VCPKG_ROOT%\vcpkg.exe install
 
-rm -r build_x64
+RMDIR /S /Q build_x64
 mkdir build_x64
 cmake -B build_x64 -S . –A x64 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows
 cmake --build .\build_x64\. --config "Release"
 
-rm -r build_x86
+RMDIR /S /Q build_x86
 mkdir build_x86
 cmake -B build_x86 -S . –A WIN32 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows
 cmake --build .\build_x86\. --config "Release"
