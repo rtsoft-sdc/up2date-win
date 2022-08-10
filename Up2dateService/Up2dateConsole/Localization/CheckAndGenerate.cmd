550691a723154df97ea895733c5e343494c62cf6
vchaplinski <107477833+vchaplinski@users.noreply.github.com>
2022-08-10 19:11:55 +0300

    test1

--- Up2dateService/Up2dateConsole/Localization/CheckAndGenerate.cmd
+++ Up2dateService/Up2dateConsole/Localization/CheckAndGenerate.cmd
@@ -6,10 +6,12 @@ set ProjectName=%5
 
 echo Check if translation %lang% is up-to-date
 copy /Y /B "%locbaml%" "%TargetDir%"
+echo "%TargetDir%locbaml.exe" /parse /check "%TargetDir%en-US\%TargetName%.resources.dll" /out:"%ProjectDir%Localization\%ProjectName%_%lang%.csv"
 "%TargetDir%locbaml.exe" /parse /check "%TargetDir%en-US\%TargetName%.resources.dll" /out:"%ProjectDir%Localization\%ProjectName%_%lang%.csv"
 IF %ERRORLEVEL% NEQ 0 GOTO end
 
 echo Generate satellite %lang% lang dll
+echo "%TargetDir%locbaml.exe" /generate "%TargetDir%en-US\%TargetName%.resources.dll" /trans:"%ProjectDir%Localization\%ProjectName%_%lang%.csv" /out:"%TargetDir%%lang%" /cul:%lang%
 "%TargetDir%locbaml.exe" /generate "%TargetDir%en-US\%TargetName%.resources.dll" /trans:"%ProjectDir%Localization\%ProjectName%_%lang%.csv" /out:"%TargetDir%%lang%" /cul:%lang%
 :end
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
