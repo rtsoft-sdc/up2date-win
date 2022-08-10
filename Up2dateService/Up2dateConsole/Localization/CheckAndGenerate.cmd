8a2b38b4b82c8e2f0a38ba75b1daa21b678c0699
vchaplinski <107477833+vchaplinski@users.noreply.github.com>
2022-08-10 20:15:22 +0300

    fix6

--- Up2dateService/Up2dateConsole/Localization/CheckAndGenerate.cmd
+++ Up2dateService/Up2dateConsole/Localization/CheckAndGenerate.cmd
@@ -4,6 +4,9 @@ set TargetName=%3
 set ProjectDir=%4
 set ProjectName=%5
 
+echo Copy locbaml.exe to target folder
+copy /Y /B "%locbaml%" "%TargetDir%"
+
 echo Generate satellite %lang% lang dll
 echo "%TargetDir%locbaml.exe" /generate "%TargetDir%en-US\%TargetName%.resources.dll" /trans:"%ProjectDir%Localization\%ProjectName%_%lang%.csv" /out:"%TargetDir%%lang%" /cul:%lang%
 "%TargetDir%locbaml.exe" /generate "%TargetDir%en-US\%TargetName%.resources.dll" /trans:"%ProjectDir%Localization\%ProjectName%_%lang%.csv" /out:"%TargetDir%%lang%" /cul:%lang%
