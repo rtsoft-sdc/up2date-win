719d09536bb3de68cd7fa84de1272f92444dc769
vchaplinski <107477833+vchaplinski@users.noreply.github.com>
2022-08-10 19:54:03 +0300

    remove check step

--- Up2dateService/Up2dateConsole/Localization/CheckAndGenerate.cmd
+++ Up2dateService/Up2dateConsole/Localization/CheckAndGenerate.cmd
@@ -4,12 +4,6 @@ set TargetName=%3
 set ProjectDir=%4
 set ProjectName=%5
 
-echo Check if translation %lang% is up-to-date
-copy /Y /B "%locbaml%" "%TargetDir%"
-echo "%TargetDir%locbaml.exe" /parse /check "%TargetDir%en-US\%TargetName%.resources.dll" /out:"%ProjectDir%Localization\%ProjectName%_%lang%.csv"
-"%TargetDir%locbaml.exe" /parse /check "%TargetDir%en-US\%TargetName%.resources.dll" /out:"%ProjectDir%Localization\%ProjectName%_%lang%.csv"
-IF %ERRORLEVEL% NEQ 0 GOTO end
-
 echo Generate satellite %lang% lang dll
 echo "%TargetDir%locbaml.exe" /generate "%TargetDir%en-US\%TargetName%.resources.dll" /trans:"%ProjectDir%Localization\%ProjectName%_%lang%.csv" /out:"%TargetDir%%lang%" /cul:%lang%
 "%TargetDir%locbaml.exe" /generate "%TargetDir%en-US\%TargetName%.resources.dll" /trans:"%ProjectDir%Localization\%ProjectName%_%lang%.csv" /out:"%TargetDir%%lang%" /cul:%lang%
