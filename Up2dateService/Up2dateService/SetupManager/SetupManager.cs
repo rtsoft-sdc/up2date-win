0fca5e70469f7e24b7340933cec298afea51177f
vchaplinski <107477833+vchaplinski@users.noreply.github.com>
2022-09-19 14:28:01 +0300

    Indicate error status in console if package installlation failed and pop-up tost notification; Suppress tost notification "package available" when user uninstalled package; Enable Install command for package with failed installation status Refactoring: get rid of redundant ProductInstallationChecker class

--- Up2dateService/Up2dateService/SetupManager/SetupManager.cs
+++ Up2dateService/Up2dateService/SetupManager/SetupManager.cs
@@ -51,7 +51,13 @@ namespace Up2dateService.SetupManager
 
         public InstallPackageResult InstallPackage(string packageFile)
         {
-            return InstallPackage(FindPackage(packageFile));
+            var package = FindPackage(packageFile);
+            var result = InstallPackage(package);
+            UpdatePackageStatus(ref package, result);
+            SafeUpdatePackage(package);
+            RefreshPackageList();
+
+            return result;
         }
 
         public void InstallPackages(IEnumerable<Package> packagesToInstall)
@@ -69,11 +75,11 @@ namespace Up2dateService.SetupManager
                 SafeUpdatePackage(package);
 
                 var result = InstallPackage(package);
-
                 UpdatePackageStatus(ref package, result);
-                eventLog.WriteEntry($"{Path.GetFileName(package.Filepath)} installation finished with result: {package.Status}");
-
                 SafeUpdatePackage(package);
+                RefreshPackageList();
+
+                eventLog.WriteEntry($"{Path.GetFileName(package.Filepath)} installation finished with result: {result}");
             }
         }
 
@@ -159,10 +165,6 @@ namespace Up2dateService.SetupManager
             {
                 WriteLogEntry(exception);
                 return InstallPackageResult.GeneralInstallationError;
-            }
-            finally
-            {
-                RefreshPackageList();
             }
         }
 
@@ -292,15 +294,22 @@ namespace Up2dateService.SetupManager
                 }
             }
 
-            ProductInstallationChecker installationChecker = new ProductInstallationChecker(installerFactory, lockedPackages);
+            RefreshinstallersProductList(lockedPackages);
 
             for (int i = 0; i < lockedPackages.Count; i++)
             {
                 Package updatedPackage = lockedPackages[i];
-                if (installationChecker.IsPackageInstalled(updatedPackage))
+
+                if (!installerFactory.IsInstallerAvailable(updatedPackage)) continue;
+
+                var installer = installerFactory.GetInstaller(updatedPackage);
+                if (installer.IsPackageInstalled(updatedPackage))
                 {
-                    installationChecker.UpdateInfo(ref updatedPackage);
-                    updatedPackage.Status = PackageStatus.Installed;
+                    if (updatedPackage.Status != PackageStatus.Installed)
+                    {
+                        installer.UpdatePackageInfo(ref updatedPackage);
+                        updatedPackage.Status = PackageStatus.Installed;
+                    }
                 }
                 else
                 {
@@ -309,7 +318,9 @@ namespace Up2dateService.SetupManager
                     updatedPackage.InstallDate = null;
                     updatedPackage.EstimatedSize = null;
                     updatedPackage.UrlInfoAbout = null;
-                    if (updatedPackage.Status != PackageStatus.Downloading && updatedPackage.Status != PackageStatus.Installing)
+                    if (updatedPackage.Status != PackageStatus.Downloading 
+                        && updatedPackage.Status != PackageStatus.Installing 
+                        && updatedPackage.Status != PackageStatus.Failed)
                     {
                         updatedPackage.Status = PackageStatus.Downloaded;
                     }
@@ -319,6 +330,18 @@ namespace Up2dateService.SetupManager
             }
 
             SafeUpdatePackages(lockedPackages);
+        }
+
+        private void RefreshinstallersProductList(IEnumerable<Package> packages)
+        {
+            var installers = new List<IPackageInstaller>();
+            foreach (IPackageInstaller installer in packages
+                .Where(p => installerFactory.IsInstallerAvailable(p))
+                .Select(p => installerFactory.GetInstaller(p))
+                .Distinct())
+            {
+                installer.Refresh();
+            }
         }
 
         private void WriteLogEntry(Exception error)
