aca3802748ee02d85310e6e18ce0d1bb2a6e7802
vchaplinski <107477833+vchaplinski@users.noreply.github.com>
2022-09-22 15:09:08 +0300

    Make logger stub output to console

--- Up2dateService/SimpleClientApp/LoggerStub.cs
+++ Up2dateService/SimpleClientApp/LoggerStub.cs
@@ -5,17 +5,31 @@ namespace SimpleClientApp
 {
     public class LoggerStub : ILogger
     {
+        private string scope;
+
+        public LoggerStub(string scope)
+        {
+            if (string.IsNullOrWhiteSpace(scope))
+            {
+                throw new ArgumentException($"'{nameof(scope)}' cannot be null or whitespace.", nameof(scope));
+            }
+
+            this.scope = scope;
+        }
+
         public ILogger SubScope(string subScope)
         {
-            return this;
+            return new LoggerStub(scope + "." + subScope);
         }
 
         public void WriteEntry(string message, Exception exception = null)
         {
+            Console.WriteLine($"{scope}: {message}\n{exception}");
         }
 
         public void WriteEntry(Exception exception)
         {
+            Console.WriteLine($"{scope}:\n{exception}");
         }
     }
 }
