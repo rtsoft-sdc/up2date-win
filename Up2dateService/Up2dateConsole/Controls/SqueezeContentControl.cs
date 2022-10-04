f7162a489e4f16db38779ef5ea52c8ef995404e7
vchaplinski <107477833+vchaplinski@users.noreply.github.com>
2022-10-04 23:45:48 +0300

    Resolve issue of overlapping controls in the bottom bar of the main window

---
+++ Up2dateService/Up2dateConsole/Controls/SqueezeContentControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Up2dateConsole.Controls
+{
+    /// <summary>
+    /// When used as a wrapper this control squeezes the child to the actually required space 
+    /// preventing the child to occupy all available space if that is not needed.
+    /// 
+    /// This control doesn't have any visible elements.
+    /// </summary>
+    public class SqueezeContentControl : ContentControl
+    {
+        private Size squeezedSize;
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            UIElement child = (UIElement)Content;
+            child.Measure(constraint);
+            squeezedSize.Width = Math.Min(child.DesiredSize.Width, constraint.Width);
+            squeezedSize.Height = Math.Min(child.DesiredSize.Height, constraint.Height);
+
+            return base.MeasureOverride(constraint);
+        }
+
+        protected override Size ArrangeOverride(Size arrangeBounds)
+        {
+            return base.ArrangeOverride(squeezedSize);
+        }
+    }
+}
