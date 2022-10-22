e5e68a624c73aab51a547002d2d75af4fbc98c4c
vchaplinski <107477833+vchaplinski@users.noreply.github.com>
2022-10-22 18:38:04 +0300

    Fix bug: notifications pop-up on each console refresh; Correct text of some messages; Detect and report on installation if Choco is not available

--- Up2dateService/Up2dateService/Installers/Choco/ChocoInstaller.cs
+++ Up2dateService/Up2dateService/Installers/Choco/ChocoInstaller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -58,6 +59,11 @@ namespace Up2dateService.Installers.Choco
                 try
                 {
                     p.Start();
+                }
+                catch (Win32Exception exception)
+                {
+                    logger.WriteEntry($"Failed to start installation of the package, perhaps Chocolatey is not available '{package.ProductName}'", exception);
+                    return InstallPackageResult.ChocoNotInstalled;
                 }
                 catch (Exception exception)
                 {
