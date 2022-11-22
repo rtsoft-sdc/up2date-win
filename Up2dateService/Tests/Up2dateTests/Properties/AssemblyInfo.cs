28fd17f3d086961adac2f3e31ed3f84cb9ae4d3e
vchaplinski <107477833+vchaplinski@users.noreply.github.com>
2022-11-22 19:07:06 +0300

    change version to 2.2.0

--- Up2dateService/Tests/Up2dateTests/Properties/AssemblyInfo.cs
+++ Up2dateService/Tests/Up2dateTests/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("db8778e5-4f26-4919-aeee-b0d776f7fd6f")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
