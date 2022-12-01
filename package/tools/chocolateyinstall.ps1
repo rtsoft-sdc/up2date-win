a249ecf685f8ad59ae4bea4b7afcca5772a72994
vchaplinski <107477833+vchaplinski@users.noreply.github.com>
2022-12-01 13:52:21 +0300

    remove checksums; replace echo with Write-Host; replace Install-ChocolateyPackage with Install-ChocolateyInstallPackage; use CDN link instead of static link for icon URL

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -10,11 +10,6 @@ $packageArgs = @{
 
   softwareName  = 'RITMS UP2DATE Client'
 
-  checksum      = 'ECBF4D47B610A6F83C2D07AE937BD8E6644B89839149FE833D93B61CAE804CC1'
-  checksumType  = 'sha256'
-  checksum64    = '65744F1E7910F73EC8B8E390E9D8908C8824F89996E717C403417B52628DF6BD'
-  checksumType64= 'sha256'
-
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
@@ -23,12 +18,12 @@ $packageArgs = @{
 [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
 if ($key.Count -eq 1) {
-	try { Stop-Process -Name "Up2dateConsole" } catch { echo "Console Was Not Closed" }  
-	try { Stop-Service -Name "Up2dateService" } catch { echo "Service Was Not Stopped" }
-	try { sc.exe delete Up2dateService } catch { echo "Service Was Not Deleted" }
+	try { Stop-Process -Name "Up2dateConsole" } catch { Write-Host "Console Was Not Closed" }  
+	try { Stop-Service -Name "Up2dateService" } catch { Write-Host "Service Was Not Stopped" }
+	try { sc.exe delete Up2dateService } catch { Write-Host "Service Was Not Deleted" }
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
 
 
 
