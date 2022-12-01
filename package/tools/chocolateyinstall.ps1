53298c96246987f37976670e8a6f00a42f5b3e4c
vchaplinski <107477833+vchaplinski@users.noreply.github.com>
2022-12-01 14:21:09 +0300

    Get rid of try-catch, use $ErrorActionPreference instead

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -7,9 +7,7 @@ $packageArgs = @{
   fileType      = 'MSI'
   file           = Get-Item $toolsDir\Up2dateSetup32.msi
   file64           = Get-Item $toolsDir\Up2dateSetup64.msi
-
   softwareName  = 'RITMS UP2DATE Client'
-
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
@@ -18,9 +16,11 @@ $packageArgs = @{
 [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
 if ($key.Count -eq 1) {
-	try { Stop-Process -Name "Up2dateConsole" } catch { Write-Host "Console Was Not Closed" }  
-	try { Stop-Service -Name "Up2dateService" } catch { Write-Host "Service Was Not Stopped" }
-	try { sc.exe delete Up2dateService } catch { Write-Host "Service Was Not Deleted" }
+	$ErrorActionPreference = "Continue"
+	Stop-Process -Name "Up2dateConsole"
+	Stop-Service -Name "Up2dateService"
+	sc.exe delete Up2dateService
+	$ErrorActionPreference = "Stop"
 }
 
 Install-ChocolateyInstallPackage @packageArgs
